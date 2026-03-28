5ea7e722fecd94cfe2998b6a6026d21256a32841
VladSKD <vladyslavpysmak5574@gmail.com>
2026-03-28 21:06:54 +0200

    Fix states: added OrderState and fixed AddEventState duplicate

--- states.py
+++ states.py
@@ -1,21 +1,14 @@
 from aiogram.fsm.state import State, StatesGroup
 
-
 class Registration(StatesGroup):
     waiting_for_last_name = State()
     waiting_for_first_name = State()
     waiting_for_institute = State()
     waiting_for_group = State()
 
-# Твій старий клас для замовлення залишається
-class AddEventState(StatesGroup):
-    title = State()
-    description = State()
-    date_time = State()
-    price = State()
-    bank_link = State()
-    card_number = State() # <-- Додали це
-    
+class OrderState(StatesGroup):
+    waiting_for_quantity = State()
+    waiting_for_proof = State()
 
 class AddEventState(StatesGroup):
     title = State()
@@ -23,3 +16,4 @@ class AddEventState(StatesGroup):
     date_time = State()
     price = State()
     bank_link = State()
+    card_number = State()
